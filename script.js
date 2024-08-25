293bda84731c8ec555fb0440a996de3b25617256
Hisham Ibrahim <mhdhishamib@gmail.com>
2024-08-25 19:01:28 +0530

    Completed the code for random machine value

---
+++ script.js
@@ -0,0 +1,23 @@
+// Generate a random value on behalf of computer (Starting)
+function getComputerChoice(){
+    let Random_value = Math.random()
+    let computerChoice;
+    if (Random_value > 0.1 && Random_value < 0.3){
+        computerChoice = "Rock"
+    }
+    else if(Random_value > 0.3 && Random_value < 0.6){
+        computerChoice = "Paper"
+    }
+    else if(Random_value > 0.6 && Random_value < 0.9){
+        computerChoice = "Scissor"
+    }
+    else if(Random_value == 0){
+        computerChoice = "Rock"
+    }
+    else{
+         computerChoice = "Scissor"
+    }
+    return computerChoice;
+}
+// Generate a random value on behalf of computer (Starting)
+
